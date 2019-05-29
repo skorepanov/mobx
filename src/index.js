cf68dba8ebd17ef83aae0f4169d039feed5a7196
Sergey Korepanov <igzhestik@mail.ru>
2019-05-29 23:38:41 +0500

    Lesson 3. Computed Values & Reactions

--- src/index.js
+++ src/index.js
@@ -3,38 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import DevTools from 'mobx-react-devtools';
-import { observable } from 'mobx';
+import { observable, computed } from 'mobx';
 import { observer } from 'mobx-react';
 
-const counterState = observable({
-  count: 0
-});
+const nickName = new class UserNickName {
+    @observable firstName = 'Yauhen';
+    @observable age = 30;
 
-counterState.increment = function() {
-  this.count++;
+    @computed get nickName() {
+        console.dir('Generate nickName!');
+        return `${this.firstName}${this.age}`;
+    }
 }
 
-counterState.decrement = function() {
-  this.count--;
+nickName.increment = function() {
+    this.age++;
+}
+
+nickName.decrement = function() {
+    this.age--;
 }
 
 @observer class Counter extends React.Component {
-  handleIncrement = () => { this.props.store.increment() };
-  handleDecrement = () => { this.props.store.decrement() };
+    handleIncrement = () => { this.props.store.increment() };
+    handleDecrement = () => { this.props.store.decrement() };
 
-  render() {
-    return (
-      <div className="App">
-        <DevTools />
-        <h1>{this.props.store.count}</h1>
-        <button onClick={this.handleDecrement}>-1</button>
-        <button onClick={this.handleIncrement}>+1</button>
-      </div>
-    );
-  }
+    render() {
+        return (
+        <div className="App">
+            <DevTools />
+            <h1>{this.props.store.nickName}</h1>
+            <h1>{this.props.store.age}</h1>
+            <button onClick={this.handleDecrement}>-1</button>
+            <button onClick={this.handleIncrement}>+1</button>
+        </div>
+        );
+    }
 }
 
-ReactDOM.render(<Counter store={counterState} />
-  , document.getElementById('root'));
+ReactDOM.render(<Counter store={nickName} />
+    , document.getElementById('root'));
 
 serviceWorker.unregister();
