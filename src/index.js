6bfdd6af6774275ba23deddf85af9497d14e21c9
Sergey Korepanov <igzhestik@mail.ru>
2019-06-02 21:13:17 +0500

    Lesson 4. Observable Object & Array

--- src/index.js
+++ src/index.js
@@ -3,45 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import DevTools from 'mobx-react-devtools';
-import { observable, computed } from 'mobx';
+import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 
-const nickName = new class UserNickName {
-    @observable firstName = 'Yauhen';
-    @observable age = 30;
+const nickName = observable({
+    firstName: 'Yauhen',
+    age: 30,
 
-    @computed get nickName() {
+    get nickName() {
         console.dir('Generate nickName!');
         return `${this.firstName}${this.age}`;
-    }
-}
+    },
 
-nickName.increment = function() {
-    this.age++;
-}
+    increment() { this.age++ },
+    decrement() { this.age-- }
+});
 
-nickName.decrement = function() {
-    this.age--;
-}
+const todos = observable([
+    { text: 'Learn React' },
+    { text: 'Learn MobX' }
+]);
 
 @observer class Counter extends React.Component {
-    handleIncrement = () => { this.props.store.increment() };
-    handleDecrement = () => { this.props.store.decrement() };
+    handleIncrement = () => { this.props.nickNameStore.increment() };
+    handleDecrement = () => { this.props.nickNameStore.decrement() };
 
     render() {
         return (
         <div className="App">
             <DevTools />
-            <h1>{this.props.store.nickName}</h1>
-            <h1>{this.props.store.age}</h1>
+            <h1>{this.props.nickNameStore.nickName}</h1>
+            <h1>{this.props.nickNameStore.age}</h1>
             <button onClick={this.handleDecrement}>-1</button>
             <button onClick={this.handleIncrement}>+1</button>
+
+            <ul>
+                {this.props.todoStore.map(({ text }) => <li key={text}>{text}</li>)}
+            </ul>
         </div>
         );
     }
 }
 
-ReactDOM.render(<Counter store={nickName} />
+ReactDOM.render(<Counter nickNameStore={nickName} todoStore={todos} />
     , document.getElementById('root'));
+
+todos.push({ text: 'Learn Redux'})
 
 serviceWorker.unregister();
